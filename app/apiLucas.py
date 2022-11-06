568b222a903a3b13127fead36a83612179a4050f
lucasdegreef <82623056+lucasdegreef@users.noreply.github.com>
2022-11-06 21:07:45 +0100

    Create apiLucas.py

---
+++ app/apiLucas.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/percentage")
+async def get_random_percentage():
+    return {"title": "https://coffee.alexflipnote.dev/random"}
